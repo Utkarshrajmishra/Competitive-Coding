165f9efaff062b3ff3b44285a35b273c626b68f2
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-14 11:45:08 +0530

    Create Valid Pair.java

---
+++ Valid Pair.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	
+	    Scanner x=new Scanner(System.in);
+		int a=x.nextInt();
+		int b=x.nextInt();
+		int c=x.nextInt();
+		if(b==a||b==c||c==a)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+	}
+}
