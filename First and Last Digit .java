0c98e144ef8dc69223237e99813c2997a4ddd8b1
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-14 11:20:29 +0530

    Create First and Last Digit .java

---
+++ First and Last Digit .java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner x=new Scanner(System.in);
+		// your code goes here
+		int t=x.nextInt();
+	  while(t-->0)
+		{
+		   int a=x.nextInt();
+		  int sum=a%10;
+		  int rev=0,k=0;
+		  while(a>0){
+		      rev=a%10;
+		      k=(k*10)+rev;
+		      a/=10;
+		  }
+		  System.out.println(sum+(k%10));
+		}
+	 
+	}
+}
