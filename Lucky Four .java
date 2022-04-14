170e3e4a00a76c12c50951bfd0d30fbe49e22b60
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-14 11:21:06 +0530

    Create  Lucky Four .java

---
+++ Lucky Four .java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner x=new Scanner(System.in);
+		// your code goes here
+		int t=x.nextInt();
+	  while(t-->0)
+		{
+		   int a=x.nextInt();
+		  int counter=0;
+		  int rev=0;
+		  while(a>0){
+		      rev=a%10;
+		      if(rev==4){
+		          counter++;
+		      }
+		      a/=10;
+		  }
+		  System.out.println(counter);
+		}
+	}
+}
