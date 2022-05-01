81c57a237aed615c7a0d8b0932f0186d27cec647
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-01 11:17:44 +0530

    Create Six Friends.java

---
+++ Six Friends.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner x=new Scanner(System.in);
+		int t=x.nextInt();
+		while(t-->0)
+		{
+		    int a=x.nextInt();
+		    int b=x.nextInt();
+		    if(a*3<b*2)
+		    {
+		        System.out.println(a*3);
+		    }
+		    else{
+		        System.out.println(b*2);
+		    }
+		}
+	}
+}
